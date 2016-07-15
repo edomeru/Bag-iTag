126b4331356cd1953412c3262a55f891244092b5
Francis Joshua Legaspi <francis.l@tektosworld.com>
2016-07-15 11:05:09 +0800

    Implement UI Testing for Sanity Check

--- TKT40232_LuggageTag_S2_UITests/TKT40232_LuggageTag_S2_UITests.swift
+++ TKT40232_LuggageTag_S2_UITests/TKT40232_LuggageTag_S2_UITests.swift
@@ -8,6 +8,31 @@
 
 import XCTest
 
+extension XCUIElement {
+  /**
+   courtesy of bay.phillips http://stackoverflow.com/questions/32821880/ui-test-deleting-text-in-text-field
+   Removes any current text in the field before typing in the new value
+   - Parameter text: the text to enter into the field
+   */
+  func clearAndEnterText(text: String) -> Void {
+    guard let stringValue = self.value as? String else {
+      XCTFail("Tried to clear and enter text into a non string value")
+      return
+    }
+    
+    self.tap()
+    
+    var deleteString: String = ""
+    for _ in stringValue.characters {
+      deleteString += "\u{8}"
+    }
+    self.typeText(deleteString)
+    
+    self.typeText(text)
+  }
+}
+
+
 class TKT40232_LuggageTag_S2_UITests: XCTestCase {
         
     override func setUp() {
@@ -29,8 +54,74 @@ class TKT40232_LuggageTag_S2_UITests: XCTestCase {
     }
     
     func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+      // Use recording to get started writing UI tests.
+      // Use XCTAssert and related functions to verify your tests produce the correct results.
+      let app = XCUIApplication()
+      app.launch()
+      
+      app.tables.buttons["add button"].tap()
+      let app2 = app
+      let luggagenameField = app2.textFields["Luggage Tag"]
+      luggagenameField.tap()
+      luggagenameField.typeText("LuggageTag")
+      app2.buttons["Return"].tap()
+      let identifierField = app2.textFields["uuid"]
+      identifierField.tap()
+      identifierField.typeText("888888888888")
+      app2.buttons["Return"].tap()
+      app2.buttons["Cancel"].tap()
+      
+      app.tables.buttons["add button"].tap()
+      
+      luggagenameField.tap()
+      luggagenameField.typeText("LuggageTag")
+      app2.buttons["Return"].tap()
+
+      identifierField.tap()
+      identifierField.typeText("888888888888")
+      app2.buttons["Return"].tap()
+      let backButton = app.navigationBars["LuggageFinder.BeaconDetailView"].buttons["back button"]
+      backButton.tap()
+      
+      app.tables.buttons["add button"].tap()
+      luggagenameField.tap()
+      luggagenameField.typeText("LuggageTag")
+      app2.buttons["Return"].tap()
+      identifierField.tap()
+      identifierField.typeText("777777777777")
+      app2.buttons["Return"].tap()
+      backButton.tap()
+       
+      let okButton = app.alerts["Error"].collectionViews.buttons["OK"]
+      okButton.tap()
+       
+      luggagenameField.tap()
+      luggagenameField.clearAndEnterText("LuggageTag2")
+      app2.buttons["Return"].tap()
+      
+      identifierField.tap()
+      identifierField.clearAndEnterText("888888888888")
+      app2.buttons["Return"].tap()
+      
+      backButton.tap()
+      okButton.tap()
+      
+      identifierField.tap()
+      identifierField.clearAndEnterText("777777777777")
+      app2.buttons["Return"].tap()
+      backButton.tap()
+      
+      app.tables.buttons["add button"].tap()
+      backButton.tap()
+      okButton.tap()
+      
+      luggagenameField.tap()
+      luggagenameField.typeText("")
+      
+      identifierField.tap()
+      identifierField.typeText("1234X6ABCDEF")
+      backButton.tap()
+      okButton.tap()
+      
     }
-    
 }
