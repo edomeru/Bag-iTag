8ed28bb6f000d4ae3e2e13fd7cc32d7a4d428f91
Francis Joshua Legaspi <francis.l@tektosworld.com>
2016-07-19 12:22:44 +0800

    Added LongTouchGesture with Drag in TableView

--- TKT40232_LuggageTag_S2/Views/ListViewController.swift
+++ TKT40232_LuggageTag_S2/Views/ListViewController.swift
@@ -11,6 +11,11 @@ import CoreBluetooth
 import CoreLocation
 import CoreData
 
+struct Drag {
+  static var placeholderView: UIView!
+  static var sourceIndexPath: NSIndexPath!
+}
+
 class ListViewController: UIViewController, CBCentralManagerDelegate, TKTCoreLocationDelegate, UITableViewDataSource,
 UITableViewDelegate, BeaconDetailViewControllerDelegate, NSFetchedResultsControllerDelegate, CustomTableCellDelegate  {
   
@@ -75,6 +80,87 @@ UITableViewDelegate, BeaconDetailViewControllerDelegate, NSFetchedResultsControl
     } catch {
       print("Failed to perform initial fecth.")
     }
+    
+    // Add LongPress Gesture in our TableView
+    let longPress = UILongPressGestureRecognizer(target: self, action: #selector(ListViewController.longPressGestureRecognized(_:)))
+    tableView.addGestureRecognizer(longPress)
+  }
+  
+  func longPressGestureRecognized(gesture: UIGestureRecognizer) {
+    let point = gesture.locationInView(tableView)
+    let indexPath = tableView.indexPathForRowAtPoint(point)
+    
+    switch gesture.state {
+    case .Began:
+      if let indexPath = indexPath {
+        let cell = tableView.cellForRowAtIndexPath(indexPath)!
+        Drag.sourceIndexPath = indexPath
+        
+        var center = cell.center
+        Drag.placeholderView = placeholderFromView(cell)
+        Drag.placeholderView.center = center
+        Drag.placeholderView.alpha = 0
+        
+        tableView.addSubview(Drag.placeholderView)
+        
+        UIView.animateWithDuration(0.25, animations: {
+          center.y = point.y
+          Drag.placeholderView.center = center
+          Drag.placeholderView.transform = CGAffineTransformMakeScale(1.05, 1.05)
+          Drag.placeholderView.alpha = 0.95
+          
+          cell.alpha = 0
+          }, completion: { (_) in
+            cell.hidden = true
+        })
+        
+      }
+    case .Changed:
+      guard let indexPath = indexPath else {
+        return
+      }
+      
+      var center = Drag.placeholderView.center
+      center.y = point.y
+      Drag.placeholderView.center = center
+      
+      if indexPath != Drag.sourceIndexPath {
+        swap(&row[indexPath.row], &row[Drag.sourceIndexPath.row])
+        tableView.moveRowAtIndexPath(Drag.sourceIndexPath, toIndexPath: indexPath)
+        Drag.sourceIndexPath = indexPath
+      }
+    default:
+      if let cell = tableView.cellForRowAtIndexPath(Drag.sourceIndexPath) {
+        cell.hidden = false
+        cell.alpha = 0
+        
+        UIView.animateWithDuration(0.25, animations: {
+          Drag.placeholderView.center = cell.center
+          Drag.placeholderView.transform = CGAffineTransformIdentity
+          Drag.placeholderView.alpha = 0
+          cell.alpha = 1
+          }, completion: { (_) in
+            Drag.sourceIndexPath = nil
+            Drag.placeholderView.removeFromSuperview()
+            Drag.placeholderView = nil
+        })
+      }
+    }
+  }
+  
+  func placeholderFromView(view: UIView) -> UIView {
+    UIGraphicsBeginImageContextWithOptions(view.bounds.size, false, 0.0)
+    view.layer.renderInContext(UIGraphicsGetCurrentContext()!)
+    let image = UIGraphicsGetImageFromCurrentImageContext() as UIImage
+    UIGraphicsEndImageContext()
+    
+    let snapshotView : UIView = UIImageView(image: image)
+    snapshotView.layer.masksToBounds = false
+    snapshotView.layer.cornerRadius = 0.0
+    snapshotView.layer.shadowOffset = CGSizeMake(-5.0, 0.0)
+    snapshotView.layer.shadowRadius = 5.0
+    snapshotView.layer.shadowOpacity = 0.4
+    return snapshotView
   }
   
   override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
