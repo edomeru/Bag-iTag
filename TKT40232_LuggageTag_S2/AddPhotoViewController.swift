aa1677e14893880816f681e0f75a293c341c97d9
Francis Joshua Legaspi <francis.l@tektosworld.com>
2017-05-17 19:27:19 +0800

    Adjust label 'Click here to take photo' in AddPhotoViewController

--- TKT40232_LuggageTag_S2/AddPhotoViewController.swift
+++ TKT40232_LuggageTag_S2/AddPhotoViewController.swift
@@ -9,74 +9,76 @@
 import UIKit
 
 protocol AddPhotoViewControllerDelegate: class {
-    
-    func addPhotoControllerDidCancel(_ controller: AddPhotoViewController)
-    
+  
+  func addPhotoControllerDidCancel(_ controller: AddPhotoViewController)
+  
 }
 
 class AddPhotoViewController: UIViewController, ModalViewControllerDelegate {
+  
+  @IBOutlet weak var nextSkipButton: CustomButton!
+  weak var delegate: AddPhotoViewControllerDelegate?
+  var photo: Data?
+  var qrCodeFrameView:UIView?
+  
+  
+  @IBOutlet weak var imgButton: CustomButton!
+  @IBOutlet weak var clickHereLabel: UILabel!
+  var isPhotoEdited = false
+  
+  
+  @IBAction func previous(_ sender: Any) {
     
-    @IBOutlet weak var nextSkipButton: CustomButton!
-    weak var delegate: AddPhotoViewControllerDelegate?
-    var photo: Data?
-    var qrCodeFrameView:UIView?
+    NotificationCenter.default.post(name: Notification.Name(rawValue: Constants.Notification.CancelPhotoView), object: nil, userInfo: nil)
+  }
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.navigationController?.isNavigationBarHidden = true
     
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    NotificationCenter.default.removeObserver(self)
+  }
+  
+  @IBAction func nextSkip(_ sender: Any) {
+    if (isPhotoEdited) {
+      photo = UIImageJPEGRepresentation(self.imgButton.currentImage!, 1.0)
+    } else {
+      photo = nil
+    }
     
-    @IBOutlet weak var imgButton: CustomButton!
-    var isPhotoEdited = false
-    
-    
-    @IBAction func previous(_ sender: Any) {
+    if let bTState =  bluetoothState {
+      if  bTState == false {
         
-        NotificationCenter.default.post(name: Notification.Name(rawValue: Constants.Notification.CancelPhotoView), object: nil, userInfo: nil)
+        NotificationCenter.default.post(name: Notification.Name(rawValue: Constants.Notification.showBluetoothWarning), object: nil, userInfo: nil)
+      } else {
+        NotificationCenter.default.post(name: Notification.Name(rawValue: Constants.Notification.SEND_PHOTO), object: photo, userInfo: nil)
+      }
+      
     }
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.navigationController?.isNavigationBarHidden = true
-        
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        NotificationCenter.default.removeObserver(self)
-    }
-    
-    @IBAction func nextSkip(_ sender: Any) {
-        if (isPhotoEdited) {
-            photo = UIImageJPEGRepresentation(self.imgButton.currentImage!, 1.0)
-        } else {
-            photo = nil
-        }
-        
-        if let bTState =  bluetoothState {
-            if  bTState == false {
-
-                NotificationCenter.default.post(name: Notification.Name(rawValue: Constants.Notification.showBluetoothWarning), object: nil, userInfo: nil)
-            } else {
-                NotificationCenter.default.post(name: Notification.Name(rawValue: Constants.Notification.SEND_PHOTO), object: photo, userInfo: nil)
-            }
-            
-        }
-    }
-    
-    
-    // MARK: ModalViewControllerDelegate
-    func didFinishPickingMediaWithInfo(_ image: UIImage) {
-        isPhotoEdited = true
-        imgButton.setImage(image, for: UIControlState())
-        imgButton.imageView?.contentMode = UIViewContentMode.center
-        nextSkipButton.setTitle("NEXT",for: .normal)
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let controller = segue.destination as! ModalViewController
-        controller.delegate = self
-    }
-    
-    deinit {
-        NotificationCenter.default.removeObserver(self)
-    }
+  }
+  
+  
+  // MARK: ModalViewControllerDelegate
+  func didFinishPickingMediaWithInfo(_ image: UIImage) {
+    isPhotoEdited = true
+    imgButton.setImage(image, for: UIControlState())
+    clickHereLabel.isHidden = true
+    imgButton.imageView?.contentMode = UIViewContentMode.center
+    nextSkipButton.setTitle("NEXT",for: .normal)
+  }
+  
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    let controller = segue.destination as! ModalViewController
+    controller.delegate = self
+  }
+  
+  deinit {
+    NotificationCenter.default.removeObserver(self)
+  }
 }
 
 
